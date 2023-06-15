c35c8c1c1a4dbeee0d9364b61b9e7df648536913
Rudy Rusli <rudy.rusli@westernpower.com.au>
2023-06-15 15:21:20 +0800

    Initial Version

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee tim = new Employee("Tim", "11/11/1985", "01/01/2020");
+        System.out.println(tim);
+        System.out.println("Age: " + tim.getAge());
+        System.out.println("Pay:" + tim.collectPay());
+
+        SalariedEmployee joe = new SalariedEmployee("Joe", "11/11/1985", "01/01/2020", 35000.00);
+        System.out.println(joe);
+        System.out.println("Joe's Paycheck = $" + joe.collectPay());
+
+        joe.retire();
+        System.out.println("Joe's pension check = $" + joe.collectPay());
+
+    }
+}
---
+++ src/Worker.java
@@ -0,0 +1,36 @@
+public class Worker {
+    private String name;
+    private String birthDate;
+    protected String endDate;
+
+    public Worker() {
+    }
+
+    public Worker(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public int getAge(){
+        int currentYear = 2025;
+        return currentYear - Integer.parseInt(this.birthDate.substring(6));
+    }
+
+    public double collectPay(){
+        return 0.0;
+    }
+
+    public void terminate(String endDate){
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "name='" + name + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                '}';
+    }
+}
+
